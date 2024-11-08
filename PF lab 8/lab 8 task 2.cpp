fbb4d641c1b92f26f1db21cd9f642ef1995357a7
Aimen-star <eimenhaque@gmail.com>
2024-11-08 19:33:41 +0500

    Add files via upload

---
+++ PF lab 8/lab 8 task 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j;
+	for(i=0;i<=5;i++)
+	{
+		{for(j=1;j<=10;j++)
+		cout<<i<<"x"<<j<<"="<<i*j<<endl;
+		cout<<endl;}
+	}
+	
+}
---
+++ PF lab 8/lab 8 task 5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,n,sum;
+	cout<<"Enter a number:";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		sum=0;
+		for(j=1;j<=i;j++)
+		sum+=j;
+		
+	}
+		cout<<sum;  
+		
+
+}
